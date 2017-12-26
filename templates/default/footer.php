502d6415a734d79cb9483bf9f8eb0c56ccec9948
Michael Kilday <mike@dragonfrugal.com>
2017-12-26 16:24:25 -0500

    v1.8.6
    -Moved the API connect timeout variable to the configuration file.
    -Small tweaks

--- templates/default/footer.php
+++ templates/default/footer.php
@@ -4,8 +4,20 @@
             	
             	<div id="api_error_alert"><?=( $_SESSION['get_data_error'] ? $_SESSION['get_data_error'] : '' )?></div>
             	
-    <p align='center'><a href='https://github.com/taoteh1221/DFD_Cryptocoin_Values/releases' target='_blank'>Version <?=$version?></a></p>
+    <p align='center'><a href='https://github.com/taoteh1221/DFD_Cryptocoin_Values/releases' target='_blank'>Version <?=$version?></a><br />(Github releases link)</p>
     
+
+    <p align='center'>Donations support further development...<br /><a id='donate' href='#' onclick='return false;'>(click to show addresses below)</a></p>
+    
+            	<div style='display: none;' id='donate_div' align='center'>
+            	
+            	BTC: <br />1FfWHekHPLH7hQcU4d5MBVQ4WekJiA8Mk2
+            	<br /><br />XMR: <br /><span class='long_linebreak'>47mWWjuwPFiPD6t2MaWcMEfejtQpMuz9oj5hJq18f7nvagcmoJwxudKHUppaWnTMPaMWshMWUTPAUX623KyEtukbSMdmpqu</span>
+            	<br /><br />ETH: <br />0xf3da0858c3cfcc28a75c1232957a7fb190d7e5e9
+            	<br /><br />STEEM: <br />taoteh1221
+            
+            	</div>
+     
     <?php
     
     // Calculate page load time
@@ -14,21 +26,9 @@
     $time = $time[1] + $time[0];
     $finish = $time;
     $total_time = round(($finish - $start), 4);
-    echo '<p align="center"> Page generated in '.$total_time.' seconds. </p>';
+    echo '<p align="center" style="color: red;"> Page generated in '.$total_time.' seconds. </p>';
     
     ?>
-
-    <p align='center'>Donations support further development...<br /><a id='donate' href='#' onclick='return false;'>(click to show addresses below)</a></p>
-    
-            <div style='display: none;' id='donate_div'' align="center">
-            
-            BTC: <br />1FfWHekHPLH7hQcU4d5MBVQ4WekJiA8Mk2
-            <br /><br />XMR: <br /><span class='long_linebreak'>47mWWjuwPFiPD6t2MaWcMEfejtQpMuz9oj5hJq18f7nvagcmoJwxudKHUppaWnTMPaMWshMWUTPAUX623KyEtukbSMdmpqu</span>
-            <br /><br />ETH: <br />0xf3da0858c3cfcc28a75c1232957a7fb190d7e5e9
-            <br /><br />STEEM: <br />taoteh1221
-            
-            </div>
-        
         
             </div>
         </div>
