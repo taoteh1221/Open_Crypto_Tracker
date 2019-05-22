98c45ac1c3297ede4ee408b9cc50b2407c87e866
Michael Kilday <mike@dragonfrugal.com>
2019-05-21 21:09:09 -0400

    Dev maintenance revisions
    Dev maintenance revisions

--- misc-docs-etc/Developing-maintaining/Git-Mirror-Coin-App-Github-To-Sourceforge.bash
+++ misc-docs-etc/Developing-maintaining/Git-Mirror-Coin-App-Github-To-Sourceforge.bash
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+#enter your build directory
 cd /home/taoteh1221/Build/
-rm -rf DFD_Cryptocoin_Values.git
 
-git clone --bare --mirror https://github.com/taoteh1221/DFD_Cryptocoin_Values.git
+#uncomment first run only
+#git clone --bare --mirror https://github.com/taoteh1221/DFD_Cryptocoin_Values.git
+
+#enter your repo directory
 cd DFD_Cryptocoin_Values.git
 
-#libsecret / gnome-keyring on ubuntu?
-#git config credential.helper store
+#uncomment first run only
+#git remote add sourceforge ssh://taoteh1221@git.code.sf.net/p/dfd-cryptocoin-values/code
 
-git remote add sourceforge ssh://taoteh1221@git.code.sf.net/p/dfd-cryptocoin-values/code
-git config remote.sourceforge.mirror true
+#uncomment first run only
+#git config remote.sourceforge.mirror true
+
+
+########################################
+#run every time to mirror original repo#
+########################################
 
 git fetch --quiet origin
 git push --quiet sourceforge
@@ -18,4 +26,4 @@ git push --quiet sourceforge
 git config alias.update-mirror '!git fetch -q origin && git push -q sourceforge'
 git update-mirror
 
-echo 'Mirror updated'
+echo 'Sourceforge Mirror updated'
