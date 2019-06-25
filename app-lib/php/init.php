02b57ec72f653d01ffa2c03e32c2fced56e1b0c2
Michael Kilday <mike@dragonfrugal.com>
2019-06-24 21:59:22 -0400

    v3.17.1 (bug fix)
    -Bug fix for Microsoft Internet Explorer read-only text input field background styling

--- app-lib/php/init.php
+++ app-lib/php/init.php
@@ -5,7 +5,7 @@
 
 //apc_clear_cache(); apcu_clear_cache(); opcache_reset();  // DEBUGGING ONLY
 
-$app_version = '3.17.0';  // 2019/JUNE/22ND
+$app_version = '3.17.1';  // 2019/JUNE/24TH
  
 date_default_timezone_set('UTC');
 
