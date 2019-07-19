7b4566638289883b85a9ecc737b3035fed04cd55
Michael Kilday <mike@dragonfrugal.com>
2019-07-19 11:34:59 -0400

    v3.25.2 (Bug Fix Release)
    -Price alert volume filter bug fix

--- app-lib/php/init.php
+++ app-lib/php/init.php
@@ -5,7 +5,7 @@
 
 //apc_clear_cache(); apcu_clear_cache(); opcache_reset();  // DEBUGGING ONLY
 
-$app_version = '3.25.1';  // 2019/JULY/16TH
+$app_version = '3.25.2';  // 2019/JULY/19TH
  
 date_default_timezone_set('UTC');
 
