68f025d7e6c3f33a5d1c6bbed5fb404bc0e77234
Michael Kilday <mike@dragonfrugal.com>
2019-08-06 18:36:11 -0400

    Version bump
    Version bump

--- app-lib/php/init.php
+++ app-lib/php/init.php
@@ -5,7 +5,7 @@
 
 //apc_clear_cache(); apcu_clear_cache(); opcache_reset();  // DEBUGGING ONLY
 
-$app_version = '3.29.2';  // 2019/AUGUST/6TH
+$app_version = '3.30.0';  // 2019/AUGUST/6TH
  
 date_default_timezone_set('UTC');
 
