23a2e4a638f3cdc53b5a40574a98d24233a72b5d
Michael Kilday <mike@dragonfrugal.com>
2019-11-26 22:20:37 -0500

    Texting service domains notes
    Texting service domains notes

--- app-lib/php/functions/general.php
+++ app-lib/php/functions/general.php
@@ -842,6 +842,45 @@ return $vars;
 ////////////////////////////////////////////////////////
 
 
+/* **************************************************
+
+AT&T: number@txt.att.net (SMS), number@mms.att.net (MMS)
+
+T-Mobile: number@tmomail.net (SMS & MMS)
+
+Verizon: number@vtext.com (SMS), number@vzwpix.com (MMS)
+
+Sprint: number@messaging.sprintpcs.com (SMS), number@pm.sprint.com (MMS)
+
+XFinity Mobile: number@vtext.com (SMS), number@mypixmessages.com (MMS)
+
+Virgin Mobile: number@vmobl.com (SMS), number@vmpix.com (MMS)
+
+Tracfone: number@mmst5.tracfone.com (MMS)
+
+Metro PCS: number@mymetropcs.com (SMS & MMS)
+
+Boost Mobile: number@sms.myboostmobile.com (SMS), number@myboostmobile.com (MMS)
+
+Cricket: number@sms.cricketwireless.net (SMS), number@mms.cricketwireless.net (MMS)
+
+Republic Wireless: number@text.republicwireless.com (SMS)
+
+Google Fi (Project Fi): number@msg.fi.google.com (SMS & MMS)
+
+U.S. Cellular: number@email.uscc.net (SMS), number@mms.uscc.net (MMS)
+
+Ting: number@message.ting.com
+
+Consumer Cellular: number@mailmymobile.net
+
+C-Spire: number@cspire1.com
+
+Page Plus: number@vtext.com
+
+**************************************************** */
+
+
 function text_email($string) {
 
 $string = explode("||",$string);
