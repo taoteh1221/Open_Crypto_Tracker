69bca2b2b380dcefd932dac46b07093da7e84dd1
Michael Kilday <mike@dragonfrugal.com>
2019-12-21 03:07:35 -0500

    UX
    UX

--- app-lib/php/functions/general.php
+++ app-lib/php/functions/general.php
@@ -358,9 +358,12 @@ $words = explode(" ",$string);
 $pretty_string = preg_replace("/btc/i", 'BTC', $pretty_string);
 $pretty_string = preg_replace("/coin/i", 'Coin', $pretty_string);
 $pretty_string = preg_replace("/exchange/i", 'Exchange', $pretty_string);
+$pretty_string = preg_replace("/market/i", 'Market', $pretty_string);
+$pretty_string = preg_replace("/forex/i", 'Forex', $pretty_string);
 $pretty_string = preg_replace("/finex/i", 'Finex', $pretty_string);
 $pretty_string = preg_replace("/stamp/i", 'Stamp', $pretty_string);
 $pretty_string = preg_replace("/flyer/i", 'Flyer', $pretty_string);
+$pretty_string = preg_replace("/panda/i", 'Panda', $pretty_string);
 
 return trim($pretty_string);
 
