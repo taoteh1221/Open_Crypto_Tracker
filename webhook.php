a7f7d05884b35c4c5e8a761dfafd09f3ce08fa54
Michael Kilday <mike@dragonfrugal.com>
2020-03-16 17:33:34 -0400

    Minor tweak before release
    Minor tweak before release

--- webhook.php
+++ webhook.php
@@ -17,7 +17,7 @@ ini_set('max_execution_time', $app_config['webhook_max_execution_time']);
 
 
 // Webhook security check (key request var must match our stored webhook key, or we abort runtime)
-if ( $_GET['key'] != $webhook_key ) {
+if ( $_REQUEST['key'] != $webhook_key ) {
 echo "Incorrect security key."
 exit;
 }
