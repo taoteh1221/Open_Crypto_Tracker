aad1637e2b0e1158578fb3e0f99563cefab9ac81
Michael Kilday <mike@dragonfrugal.com>
2020-05-15 18:11:23 -0400

    Rsync script for public demo maintainence
    Rsync script for public demo maintainence

--- DOCUMENTATION-ETC/Developing-maintaining/scripts/rsync-mirror.bash
+++ DOCUMENTATION-ETC/Developing-maintaining/scripts/rsync-mirror.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 rsync -vaz ~/public_html/DEV-SITE/cache/charts ~/public_html/PUBLIC-SITE/cache
+
+rsync -vaz ~/public_html/DEV-SITE/cache/secured/external_api ~/public_html/PUBLIC-SITE/cache/secured
