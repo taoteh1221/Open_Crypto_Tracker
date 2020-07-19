c9d1a442a1ea491207d5d36e9cb3365473e32118
Michael Kilday <mike@dragonfrugal.com>
2020-07-19 16:10:24 -0400

    Admin area UX
    Admin area UX

--- admin.php
+++ admin.php
@@ -59,12 +59,6 @@ require("templates/interface/php/header.php");
 			<a class="nav-link admin_change_width" data-toggle="tab" data-width="fixed_max" href="#admin_power_user" role="tab" aria-controls="admin_power_user">Power User</a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link admin_change_width active" data-toggle="tab" data-width="fixed_max" href="#admin_api" role="tab" aria-controls="admin_api">API</a>
-		  </li>
-		  <li class="nav-item">
-			<a class="nav-link admin_change_width" data-toggle="tab" data-width="fixed_max" href="#admin_webook" role="tab" aria-controls="admin_webook">Webhook</a>
-		  </li>
-		  <li class="nav-item">
 			<a class="nav-link admin_change_width" data-toggle="tab" data-width="fixed_max" href="#admin_text_gateways" role="tab" aria-controls="admin_text_gateways">Text Gateways</a>
 		  </li>
 		  <li class="nav-item">
@@ -74,10 +68,10 @@ require("templates/interface/php/header.php");
 			<a class="nav-link admin_change_width" data-toggle="tab" data-width="fixed_max" href="#admin_developer_only" role="tab" aria-controls="admin_developer_only">Developer Only</a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link admin_change_width" data-toggle="tab" data-width="fixed_max" href="#admin_backup_restore" role="tab" aria-controls="admin_backup_restore">Backup / Restore</a>
+			<a class="nav-link admin_change_width active" data-toggle="tab" data-width="fixed_max" href="#admin_api" role="tab" aria-controls="admin_api">API</a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link admin_change_width" data-toggle="tab" data-width="fixed_max" href="#admin_reset" role="tab" aria-controls="admin_reset">Reset</a>
+			<a class="nav-link admin_change_width" data-toggle="tab" data-width="fixed_max" href="#admin_webook" role="tab" aria-controls="admin_webook">Webhook</a>
 		  </li>
 		  <li class="nav-item">
 			<a class="nav-link admin_change_width" data-toggle="tab" data-width="full" href="#system_charts" role="tab" aria-controls="system_charts">System Charts</a>
@@ -87,6 +81,12 @@ require("templates/interface/php/header.php");
 		  </li>
 		  <li class="nav-item">
 			<a class="nav-link admin_change_width" data-toggle="tab" data-width="full" href="#admin_logs" role="tab" aria-controls="admin_logs">App Logs</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link admin_change_width" data-toggle="tab" data-width="fixed_max" href="#admin_backup_restore" role="tab" aria-controls="admin_backup_restore">Backup / Restore</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link admin_change_width" data-toggle="tab" data-width="fixed_max" href="#admin_reset" role="tab" aria-controls="admin_reset">Reset</a>
 		  </li>
 		</ul>
 		
@@ -113,14 +113,6 @@ require("templates/interface/php/header.php");
 			<?php require("templates/interface/php/admin/admin-sections/power-user.php"); ?>
 		  </div>
 		  
-		  <div class="tab-pane active" id="admin_api" role="tabpanel">
-			<?php require("templates/interface/php/admin/admin-sections/api.php"); ?>
-		  </div>
-		  
-		  <div class="tab-pane" id="admin_webook" role="tabpanel">
-			<?php require("templates/interface/php/admin/admin-sections/webhook.php"); ?>
-		  </div>
-		  
 		  <div class="tab-pane" id="admin_text_gateways" role="tabpanel">
 			<?php require("templates/interface/php/admin/admin-sections/text-gateways.php"); ?>
 		  </div>
@@ -133,12 +125,12 @@ require("templates/interface/php/header.php");
 			<?php require("templates/interface/php/admin/admin-sections/developer-only.php"); ?>
 		  </div>
 		  
-		  <div class="tab-pane" id="admin_backup_restore" role="tabpanel">
-			<?php require("templates/interface/php/admin/admin-sections/backup-restore.php"); ?>
+		  <div class="tab-pane active" id="admin_api" role="tabpanel">
+			<?php require("templates/interface/php/admin/admin-sections/api.php"); ?>
 		  </div>
 		  
-		  <div class="tab-pane" id="admin_reset" role="tabpanel">
-			<?php require("templates/interface/php/admin/admin-sections/reset.php"); ?>
+		  <div class="tab-pane" id="admin_webook" role="tabpanel">
+			<?php require("templates/interface/php/admin/admin-sections/webhook.php"); ?>
 		  </div>
 		  
 		  <div class="tab-pane" id="system_charts" role="tabpanel">
@@ -151,6 +143,14 @@ require("templates/interface/php/header.php");
 		  
 		  <div class="tab-pane" id="admin_logs" role="tabpanel">
 			<?php require("templates/interface/php/admin/admin-sections/app-logs.php"); ?>
+		  </div>
+		  
+		  <div class="tab-pane" id="admin_backup_restore" role="tabpanel">
+			<?php require("templates/interface/php/admin/admin-sections/backup-restore.php"); ?>
+		  </div>
+		  
+		  <div class="tab-pane" id="admin_reset" role="tabpanel">
+			<?php require("templates/interface/php/admin/admin-sections/reset.php"); ?>
 		  </div>
 		  
 		</div>
