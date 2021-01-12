5f38456a8d0166a75cd826565f0ec01636cd193b
Michael Kilday <mike@dragonfrugal.com>
2021-01-12 09:02:54 -0500

    Code optimization
    Code optimization

--- app-lib/php/other/sub-init/interface-sub-init.php
+++ app-lib/php/other/sub-init/interface-sub-init.php
@@ -103,7 +103,8 @@ $show_feeds = explode(',', rtrim( ( $_POST['show_feeds'] != '' ? $_POST['show_fe
 		$scan_charts = array_map('strip_underscore_and_after', $scan_charts); // Strip underscore, and everything after
 		$loop = 0;
 		foreach ($scan_charts as $market_key) {
-			if ( array_key_exists($market_key, $app_config['charts_alerts']['tracked_markets']) ) {
+			// IF asset exists in charts app config, AND $show_charts UI key format is latest iteration (fiat conversion charts USED TO have no underscore)
+			if ( array_key_exists($market_key, $app_config['charts_alerts']['tracked_markets']) && stristr($show_charts[$loop], '_') ) {
 			$temp_show_charts[$loop] = $show_charts[$loop];
 			}
 		$loop = $loop + 1;
