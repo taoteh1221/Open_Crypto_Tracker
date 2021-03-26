34e0539933cd3039612c71ea99986afc020f7060
Michael Kilday <mike@dragonfrugal.com>
2021-03-26 17:00:51 -0400

    Revised auto-install
    Revised auto-install

--- FOLIO-INSTALL.bash
+++ FOLIO-INSTALL.bash
@@ -199,13 +199,13 @@ echo "#WHEN YOU FIRST RUN THIS APP AFTER INSTALLATION#. IT'S #HIGHLY RECOMMENDED
 echo "ESPECIALLY ON PUBLIC FACING / KNOWN SERVERS, #OR SOMEBODY ELSE MAY BEAT YOU TO IT#."
 echo " "
 
-echo "PLEASE REPORT ANY ISSUES HERE: https://github.com/taoteh1221/Open_Crypto_Portfolio_Tracker/issues"
-echo " "
-
-echo "!!IMPORTANT NOTICE!!: This auto-install script is ONLY FOR SELF-HOSTED ENVIRONMENTS, THAT #DO NOT#"
+echo "!!IMPORTANT INSTALL NOTICE!!: This auto-install script is ONLY FOR SELF-HOSTED ENVIRONMENTS, THAT #DO NOT#"
 echo "ALREADY HAVE A WEB SERVER OR CONTROL PANEL INSTALLED ON THE SYSTEM. If this is a managed hosting"
 echo "environment that a service provider has already provisioned, please quit this auto-install session,"
 echo "and refer to the \"Manual Install\" section of the README.txt file documentation."
+echo " "
+
+echo "PLEASE REPORT ANY ISSUES HERE: https://github.com/taoteh1221/Open_Crypto_Portfolio_Tracker/issues"
 echo " "
   				
 echo "Select 1 or 2 to choose whether to continue with installation, or quit."
