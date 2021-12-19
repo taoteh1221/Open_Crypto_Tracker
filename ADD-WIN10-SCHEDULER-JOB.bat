77a17846dfbe599310ae5a0706dd6f5a4685927d
taoteh1221 <mike@dragonfrugal.com>
2021-12-19 16:57:06 -0500

    Revised win10 task scheduler support
    Revised win10 task scheduler support

--- ADD-WIN10-SCHEDULER-JOB.bat
+++ ADD-WIN10-SCHEDULER-JOB.bat
@@ -1,40 +1,65 @@
 @echo off
 
+echo:
+echo Enter the time interval in minutes to run this scheduled task 
+set /p cron_interval="(5, 10, 15, 20, or 30...leave blank / hit enter for default of 20): "
 
-set /p cron_interval=Enter the time interval in minutes to run this scheduled task (5, 10, 15, 20, or 30...leave blank / hit enter for default of 20):
-
-IF "%cron_interval%" == "" (
-set cron_interval=20
-echo Using default time interval (in minutes): %cron_interval%
-) ELSE (
-echo Using custom time interval (in minutes): %cron_interval%
+if "%cron_interval%"=="" (
+set "cron_interval=20"
+echo:
+echo Using default time interval in minutes: 
+) else (
+echo:
+echo Using custom time interval in minutes: 
 )
 
+echo %cron_interval%
 echo:
 
+echo Enter the full path to the PHP CLI binary 
+set /p php_cli_binary="(leave blank / hit enter for default of C:/xampp/php/php-win.exe): "
 
-set /p php_cli_binary=Enter the full path to the PHP CLI binary (leave blank / hit enter for default of C:/xampp/php/php-win.exe):
-
-IF "%php_cli_binary%" == "" (
-set php_cli_binary=C:/xampp/php/php-win.exe
-echo Using default PHP CLI binary path: %php_cli_binary%
-) ELSE (
-echo Using custom PHP CLI binary path: %php_cli_binary%
+if "%php_cli_binary%"=="" (
+set "php_cli_binary=C:/xampp/php/php-win.exe"
+echo:
+echo Using default PHP CLI binary path: 
+) else (
+echo:
+echo Using custom PHP CLI binary path: 
 )
 
+echo %php_cli_binary%
 echo:
 
+echo Enter the full path to cron.php 
+set /p cron_php="(leave blank / hit enter for default of C:/xampp/htdocs/cron.php): "
 
-set /p cron_php=Enter the full path to cron.php (leave blank / hit enter for default of C:/xampp/htdocs/cron.php):
-
-IF "%cron_php%" == "" (
-set cron_php=C:/xampp/htdocs/cron.php
-echo Using default cron.php path: %cron_php%
-) ELSE (
-echo Using custom cron.php path: %cron_php%
+if "%cron_php%"=="" (
+set "cron_php=C:/xampp/htdocs/cron.php"
+echo:
+echo Using default cron.php path: 
+) else (
+echo:
+echo Using custom cron.php path: 
 )
 
+echo %cron_php%
+echo:
+
+echo Windows will now add this Task Scheduler entry. If you wish
+echo to remove this entry in the future, open Task Scheduler and
+echo choose 'CRYPTO_TRACKER_CRON', then click 'Delete' on the right.
+echo:
+
+pause
 echo:
 
 schtasks /Create /TN CRYPTO_TRACKER_CRON /TR "%php_cli_binary% %cron_php%" /SC MINUTE /MO %cron_interval%
+
+echo:
+
+echo Task 'CRYPTO_TRACKER_CRON' has been created, with PHP CLI binary path set as '%php_cli_binary%', cron path set as '%cron_php%', and set to run every %cron_interval% minutes.
+
+echo:
+
 pause
