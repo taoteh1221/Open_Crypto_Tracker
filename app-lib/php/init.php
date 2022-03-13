86ca037eb3c3e18ae8f4bb277049f1a2a9357099
taoteh1221 <mike@dragonfrugal.com>
2022-03-13 15:17:49 -0400

    version bump
    version bump

--- app-lib/php/init.php
+++ app-lib/php/init.php
@@ -10,7 +10,7 @@
 
 
 // Application version
-$app_version = '5.14.1';  // 2022/MARCH/11TH
+$app_version = '5.15.0';  // 2022/MARCH/13TH
 
 
 // Detect if we are running the desktop or server edition
