a5d89aa5bf4588e8c0ce0289cdd7108c38fd4f90
taoteh1221 <mike@dragonfrugal.com>
2022-03-15 04:41:45 -0400

    docs
    docs

--- DOCUMENTATION-ETC/RASPBERRY-PI/EASTER-EGG.bash
+++ DOCUMENTATION-ETC/RASPBERRY-PI/EASTER-EGG.bash
@@ -1,6 +1,26 @@
 #!/bin/bash
 
+
+########################################################################################################################
+########################################################################################################################
+
+
 # Copyright 2022 GPLv3, Bluetooth Internet Radio By Mike Kilday: Mike@DragonFrugal.com
+
+# https://github.com/taoteh1221/Bluetooth_Internet_Radio
+
+# Fully automated setup of bluetooth and an internet radio player (PyRadio), on a headless RaspberryPi / DietPi,
+# connecting to a stereo system's bluetooth receiver (bash script, chmod +x it to run).
+
+# To install automatically on Ubuntu / DietPi OS / RaspberryPi OS, copy => paste => run the command below in a
+# terminal program (using the 'Terminal' app in the system menu, or over remote SSH), while logged in AS THE
+# USER THAT WILL RUN THE APP (user must have sudo privileges):
+
+# wget --no-cache -O bt-radio-setup.bash https://tinyurl.com/bt-radio-setup;chmod +x bt-radio-setup.bash;./bt-radio-setup.bash
+
+
+########################################################################################################################
+########################################################################################################################
 
 
 # Version of this script
