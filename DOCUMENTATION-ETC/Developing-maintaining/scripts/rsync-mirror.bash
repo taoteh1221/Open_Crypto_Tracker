c53747c2adf697a2d4b2365984c169f022bfa4b2
taoteh1221 <mike@dragonfrugal.com>
2022-04-11 12:53:55 -0400

    remove old maintenance script
    remove old maintenance script

--- DOCUMENTATION-ETC/Developing-maintaining/scripts/rsync-mirror.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rsync -vaz ~/public_html/DEV-SITE/cache/charts ~/public_html/PUBLIC-SITE/cache
-
-rsync -vaz ~/public_html/DEV-SITE/cache/secured/external_data ~/public_html/PUBLIC-SITE/cache/secured
