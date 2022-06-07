ef605526d1f290dd381752d54d70f787d902c65f
taoteh1221 <mike@dragonfrugal.com>
2022-06-07 16:48:04 -0400

    fix
    fix

--- templates/interface/desktop/php/admin/admin-sections/developer.php
+++ templates/interface/desktop/php/admin/admin-sections/developer.php
@@ -6,6 +6,9 @@
 
 ?>
 	
+	<div class='bitcoin align_center' style='margin-bottom: 20px;'>(advanced configuration, handle with care)</div>
+				
+				
 	<p> Coming Soon&trade; </p>
 				
 	<p class='bitcoin'> Editing these settings is <i>currently only available manually</i>, by updating the file config.php (in this app's main directory: <?=$base_dir?>) with a text editor.</p>
