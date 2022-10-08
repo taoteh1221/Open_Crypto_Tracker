b14c83909671119369e6bdc047ac6cef96963076
taoteh1221 <mike@dragonfrugal.com>
2022-10-07 22:09:32 -0400

    Documentation on troubleshooting LINUX Desktop Edition startup issues
    Documentation on troubleshooting LINUX Desktop Edition startup issues

--- templates/interface/desktop/php/user/user-sections/help.php
+++ templates/interface/desktop/php/user/user-sections/help.php
@@ -709,6 +709,82 @@ Running a google search for "set permanently ulimit -n linux", you'll find tons 
 	  
 	
 	<?php
+	$accord_var = 'shared_library_issues';
+	?>
+	
+	  <div class="card z-depth-0 bordered">
+	    <div class="card-header" id="heading_<?=$accord_var?>">
+	      <h5 class="mb-0">
+	        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse_<?=$accord_var?>"
+	          aria-expanded="false" aria-controls="collapse_<?=$accord_var?>">
+	          
+	          LINUX Desktop Edition Error: "CGI program sent malformed or too big"
+	          
+	        </button>
+	      </h5>
+	    </div>
+	    <div id="collapse_<?=$accord_var?>" class="faq_bg collapse" aria-labelledby="heading_<?=$accord_var?>"
+	      data-parent="#accordionHelp">
+	      <div class="card-body">
+	      
+	         
+	       If you are using the LINUX Desktop Edition, and you get an error at startup like "CGI program sent malformed or too big", you probably need to compile a custom "php-cgi" binary file on the system you are using, and replace the "php-cgi-custom" binary in the Desktop Edition main folder. Sometimes compiled PHP binaries aren't very portable between different linux systems. To see if this is really the problem by command-line, open a terminal and use the "cd" (change directory) command to go to the main directory of the Desktop Edition, and then type this command:
+	    <br /><br />
+
+<pre class='rounded' style='display: inline-block;<?=( $ct_gen->is_msie() == false ? ' padding-top: 1em !important;' : '' )?>'><code class='hide-x-scroll less' style='white-space: nowrap; width: auto; display: inline-block;'>./php-cgi-custom INSTALL_CRYPTO_TRACKER_HERE/index.php</code></pre>
+	    <br /><br />
+
+If you see an error like this below, you system is NOT compatible with the included "php-cgi-custom" PHP binary, and you'll need to build a new one for your system:
+	    <br /><br />
+
+<pre class='rounded' style='display: inline-block;<?=( $ct_gen->is_msie() == false ? ' padding-top: 1em !important;' : '' )?>'><code class='hide-x-scroll less' style='white-space: nowrap; width: auto; display: inline-block;'>./php-cgi-custom: error while loading shared libraries: XXXXX.so.X: cannot open shared object file: No such file or directory</code></pre>
+	    <br /><br />
+
+Documentation for building custom PHP binaries on linux can be found here (as well as the source code to download to build it with):
+	    <br /><br />
+
+<a href='https://github.com/php/php-src/blob/master/README.md' target='_blank'>https://github.com/php/php-src/blob/master/README.md</a>
+	    <br /><br />
+
+Here is the SPECIFIC "./configure" command (mentioned in the above documentation link) you will need to build PHP with the REQUIRED extensions that the Desktop Edition of this crypto tracker app NEEDS:
+	    <br /><br />
+
+<pre class='rounded'><code class='hide-x-scroll less' style='width: 100%; height: 500px;'>./configure \
+  --enable-bcmath \
+  --enable-gd \
+  --enable-calendar \
+  --enable-dba \
+  --enable-exif \
+  --enable-ftp \
+  --enable-fpm \
+  --enable-mbstring \
+  --enable-shmop \
+  --enable-sigchild \
+  --enable-soap \
+  --enable-sockets \
+  --enable-sysvmsg \
+  --with-libdir=lib64 \
+  --with-zip \
+  --with-bz2 \
+  --with-curl \
+  --with-gettext \
+  --with-openssl \
+  --with-pdo-mysql \
+  --with-zlib \
+  --with-libxml \
+  --with-freetype \
+  --prefix=$HOME/customphp</code></pre>
+	    <br /><br />
+  
+After using the above configuration, and then running "make", when you then run "make install" AFTERWARDS, your custom PHP binaries with be installed to a new directory in your home folder called "customphp". Inside this folder you will find a subdirectory named "bin". Inside this subdirectory you'll find the custom PHP binary named "php-cgi". Copy this file "php-cgi" over into the main directory of your linux Desktop Edition of the crypto tracker app. Now delete the old "php-cgi-custom" file in there, and rename the new "php-cgi" file to be named "php-cgi-custom" instead. The linux Desktop Edition of this crypto tracker app should now work fine, if it was indeed a shared library problem.
+	        
+	        
+	      </div>
+	    </div>
+	  </div>
+	  
+	
+	<?php
 	$accord_var = 'custom_plugins';
 	?>
 	
