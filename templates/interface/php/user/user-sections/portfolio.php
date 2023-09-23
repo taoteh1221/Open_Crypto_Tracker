98122c3b81ddb066beef717ce20c0b2da325a51e
taoteh1221 <mike@dragonfrugal.com>
2023-09-23 16:21:31 -0400

    final v6.00.27 review (#90)
    final v6.00.27 review

--- templates/interface/php/user/user-sections/portfolio.php
+++ templates/interface/php/user/user-sections/portfolio.php
@@ -1780,14 +1780,6 @@ var server_header_defaults_content = '<h5 class="yellow tooltip_title">Average S
     		
     		
    <ul>
-	
-        <?php
-        if ( $ct['app_edition'] == 'desktop' ) {
-        ?>
-	<li class='red' style='font-weight: bold;'>Using 'zoom' (top right) can skew chart hovering mouse positions.</li>	
-        <?php
-        }
-        ?>
         
 	
 	<li class='bitcoin' style='font-weight: bold;'>Charts may take awhile to update with the latest data.</li>	
