77499ebe8ca8348deeb3570f46463a822c7c6189
taoteh1221 <mike@dragonfrugal.com>
2023-10-06 21:09:18 -0400

    last minute fixes / tweaks
    last minute fixes / tweaks

--- templates/interface/php/admin/admin-sections/power-user.php
+++ templates/interface/php/admin/admin-sections/power-user.php
@@ -28,7 +28,7 @@ else {
 	
 	<p class='bitcoin bitcoin_dotted'>
 	
-	These sections / category pages will be INCREMENTALLY populated with the corrisponding admin configuration options, over a period of time AFTER the initial v6.00.x releases (versions 6.00.x will only test the back-end / under-the-hood stability of HIGH / ENHANCED / NORMAL MODES of the Admin Interface security levels). <br /><br />You may need to turn off "Enhanced" OR "Normal" mode of the Admin Interface security level (at the top of the "Security" section in this admin area), to edit any UNFINISHED SECTIONS by hand in the config files (config.php in the app install folder, and any plug-conf.php files in the plugins folders).
+	These sections / category pages will be INCREMENTALLY populated with the corrisponding admin configuration options, over a period of time AFTER the initial v6.00.x releases (versions 6.00.x will only test the back-end / under-the-hood stability of NORMAL / MEDIUM / HIGH MODES of the Admin Interface security levels). <br /><br />You may need to turn off "Normal" OR "Medium" mode of the Admin Interface security level (at the top of the "Security" section in this admin area), to edit any UNFINISHED SECTIONS by hand in the config files (config.php in the app install folder, and any plug-conf.php files in the plugins folders).
 	
 	</p>
 	
