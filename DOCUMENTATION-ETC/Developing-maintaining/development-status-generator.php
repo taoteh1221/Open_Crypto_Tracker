d840a84dc97a1883f10ab3b1976cf94f054956cf
taoteh1221 <mike@dragonfrugal.com>
2024-12-21 12:09:23 -0500

    tweaks
    tweaks

--- DOCUMENTATION-ETC/Developing-maintaining/development-status-generator.php
+++ DOCUMENTATION-ETC/Developing-maintaining/development-status-generator.php
@@ -2,8 +2,13 @@
 
 $dev_status = array();
 
+// DUMMY ENTRIES, TO PROPERLY FORMAT AS AN MULTI-DIMENSIONAL ARRAY WHEN CONVERTED TO JSON (EVEN IF NO REAL ENTRIES EXISTS)
+$dev_status[] = array('dummy_entry' => true);
+$dev_status[] = array('dummy_entry' => true);
+
 
 ///////////////////////////////////////////////////////////////////
+
 
 // NEW ENTRY
 $dev_status[] = array(
@@ -19,6 +24,23 @@ $dev_status[] = array(
                    
                    // DESCRIPTION
                    'affected_desc' => 'I\'m now aware of @JupiterExchange COMPLETELY SCRAPPING their V1 PRICE API, right after my HEAVY integration of their SEARCH API in v6.00.39 of Open #Crypto Tracker. 😩 I will be upgrading to their V2 PRICE API, in the upcoming v6.01.0 release.',
+
+                   );
+
+// NEW ENTRY
+$dev_status[] = array(
+
+                   // HUMAN-READABLE DATE, CONVERTED TO A TIMESTAMP
+                   'timestamp' => strtotime('2024-11-28'),
+
+                   // HIGHEST VERSION AFFECTED
+                   'affected_version' => '6.00.40',
+
+                   // DOES THIS AFFECT EARLIER VERSIONS
+                   'affected_earlier' => false,
+                   
+                   // DESCRIPTION
+                   'affected_desc' => 'You can now download your price chart and app config backups, on the "Reset / Backup & Restore" ADMIN page. In an UPCOMING (unknown when) release, you will be able to RESTORE these backups on the same admin page.',
 
                    );
 
