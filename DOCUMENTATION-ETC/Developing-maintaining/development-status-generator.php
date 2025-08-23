3293a38a223781095242bc5cb1b70601ebea6084
taoteh1221 <mike@dragonfrugal.com>
2025-08-23 16:20:42 -0400

    windows desktop edition testing
    windows desktop edition testing

--- DOCUMENTATION-ETC/Developing-maintaining/development-status-generator.php
+++ DOCUMENTATION-ETC/Developing-maintaining/development-status-generator.php
@@ -25,7 +25,7 @@ $dev_status[] = array(
                    'affected_earlier' => false,
                    
                    // DESCRIPTION
-                   'affected_desc' => 'v6.01.05 has been released, which FIXES occasionally getting a blank page in the user area, related to automated CoinGecko.com multi-currency support (added in v6.01.04). API throttling has also been further refined, for better API data reliability. Other smaller fixes and user experience improvements are also included.',
+                   'affected_desc' => 'v6.01.05 has been released, which FIXES occasionally getting a blank page in the user area, related to automated CoinGecko.com multi-currency support (added in v6.01.04). API throttling has also been further refined, for better API data reliability. Other optimizations / smaller fixes / user experience improvements are also included.',
 
                    );
 
