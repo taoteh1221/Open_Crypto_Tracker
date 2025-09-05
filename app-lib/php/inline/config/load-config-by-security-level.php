d84767b2e372d64851ee409573dc3dacc4ab559a
taoteh1221 <mike@dragonfrugal.com>
2025-09-05 12:52:08 -0400

    semantics, optimizations
    semantics, optimizations

--- app-lib/php/inline/config/load-config-by-security-level.php
+++ app-lib/php/inline/config/load-config-by-security-level.php
@@ -28,14 +28,14 @@ require($ct['base_dir'] . '/app-lib/php/inline/init/plugins-init.php');
 
 // Add PLUGIN version states to DEFAULT config (for SAFETY on any imported cached configs [IF high sec mode ever disabled in future])
 // (AFTER initializing plugins / BEFORE any initial default conf comparison digest)
-$default_ct_conf = $ct['gen']->sync_version_states($default_ct_conf, true); // Runs quick, as we don't need comparison checks
+$ct['default_conf'] = $ct['gen']->config_versioning($ct['default_conf'], true); // Runs quick, as we don't need comparison checks
 
 
 // If no comparison digest of the default config yet, save it now to the cache
 // (MUST be done AFTER registering active plugins)
 // !!!!ONLY if not set YET, as it's for tracking state change when loading config in high security mode!!!!
 if ( $check_default_ct_conf == null ) {
-$check_default_ct_conf = md5( serialize($default_ct_conf) );
+$check_default_ct_conf = md5( serialize($ct['default_conf']) );
 $ct['cache']->save_file($ct['base_dir'] . '/cache/vars/state-tracking/default_conf_md5.dat', $check_default_ct_conf);
 }
 
@@ -43,7 +43,7 @@ $ct['cache']->save_file($ct['base_dir'] . '/cache/vars/state-tracking/default_co
 // Check PLUGIN version states, in medium / normal admin security modes (for SAFETY on any imported cached configs)
 // (AFTER force-set above on DEFAULT config [so we have plugin defaults set, if needed])
 if ( $ct['admin_area_sec_level'] != 'high' && !$ct['reset_config'] ) {
-$ct['conf'] = $ct['gen']->sync_version_states($ct['conf']);
+$ct['conf'] = $ct['gen']->config_versioning($ct['conf']);
 }
 
 
