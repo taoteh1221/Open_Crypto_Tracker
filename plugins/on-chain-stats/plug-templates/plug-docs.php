a22a127a48e47a73eb3587c9afaa158840936390
taoteh1221 <mike@dragonfrugal.com>
2025-11-23 21:04:03 -0500

    TODO, docs
    TODO, docs

--- plugins/on-chain-stats/plug-templates/plug-docs.php
+++ plugins/on-chain-stats/plug-templates/plug-docs.php
@@ -6,4 +6,6 @@
 
 ?>
 
-<p class='bitcoin'>This plugin is a work-in-progress, as it's still in the early development phase.</p>
+<p>This plugin currently (as of v0.91.00) supports Bitcoin and Solana on-chain stats charts / maps in the user interface, under the plugin menu. In the future (coming soon&trade;), there will be webhook support as well.</p>
+
+<p>Bitcoin does NOT support revealing node TYPE, beyond P2P-related capabilities, so that's the only node-type-related filter we have in the interface (see <a href='https://github.com/bitcoin/bitcoin/issues/33930#issuecomment-3568645276' target='_BLANK'>Peter Wuille's comment on GitHub</a>).</p>
